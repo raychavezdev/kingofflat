171bcba7a1c0af92257f11abc5f6fcea037b1f74
Raymundo Chavez <raymundo.developer@gmail.com>
2025-02-13 19:03:42 -0600

    Cambia foto del banner

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -3,7 +3,7 @@ import Container from "../../layouts/Container";
 const Banner = () => {
   return (
     <div className="h-96 md:h-[700px] bg-[url(/foto1.jpg)] bg-cover bg-center mt-28 md:mt-24 z-30">
-      <div className="flex items-center h-full  bg-black/50">
+      <div className="flex items-center h-full  bg-black/60">
         <Container className="text-center">
           <h1 className="text-clampTitle font-sigmar tracking-widest">
             KING OF FLAT
